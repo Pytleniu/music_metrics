e133f865846cf062b9b91bdd6ad2cbee45cef776
michał kopeć <Michalkopec1111@wp.pl>
2024-01-10 02:33:40 +0100

    Comment for all metrics and tests for different input type

--- music_metrics/src/pitch_metrics.py
+++ music_metrics/src/pitch_metrics.py
@@ -5,13 +5,37 @@ import matplotlib.pyplot as plt
 import numpy as np
 from collections import defaultdict
 
-from utils import load_representations
+from .utils import load_representations
+
+pitch_class = {
+    0: 'C',
+    1: 'C#',
+    2: 'D',
+    3: 'D#',
+    4: 'E',
+    5: 'F',
+    6: 'F#',
+    7: 'G',
+    8: 'G#',
+    9: 'A',
+    10: 'A#',
+    11: 'B'
+}
 
 def plot_pitch_class_histogram(histogram):
     plt.bar(np.arange(12), histogram)
     plt.xticks(np.arange(12), ['C', '', 'D', '', 'E', 'F', '', 'G', '', 'A', '', 'B'])
     plt.xlabel('Note')
     plt.ylabel('Proportion')
+
+def plot_chromagram(chromagram):
+    plt.figure(figsize=(12, 8))
+    plt.imshow(chromagram, aspect='auto', origin='lower', cmap='viridis')
+    plt.colorbar(label='Intensity')
+    plt.xlabel('Time (in 1/fs seconds)')
+    plt.ylabel('Pitch Class (C, C#, D, ..., B)')
+    plt.title('Chromagram')
+    plt.show()
 
 def compute_best_scale(muspy_representation):
     roots = [i for i in range(12)]
@@ -52,33 +76,33 @@ def get_pitch_metrics(data: any):
         chroma = midi_representation.get_chroma()
 
     # pypianoroll
-    # if pianoroll_representation:
-    #     print(type(pianoroll_representation))    
-    #     pitch_range_tuple = pypianoroll.pitch_range_tuple(pianoroll_representation)
+    # if pianoroll_representation.size > 0:
+        # pitch_range_tuple = pypianoroll.pitch_range_tuple(pianoroll_representation)
+
+    major_class_scale = (pitch_class[major_scale[0]], major_scale[1])
+    minor_class_scale = (pitch_class[minor_scale[0]], minor_scale[1])
 
     metrics_table = PrettyTable()
     metrics_table.field_names = ['Metric', 'Value', 'Description']
 
     metric_descriptions = {
-        'pitch_range': "Range of pitches used",
-        'pitch_range_tuple': "Tuple of pitches range",
+        'pitch_range': "the difference between the maximum pitch value and the minimum pitch value",
         'n_pitches_used': "Number of different pitches used",
         'n_pitch_classes_used': "Number of different pitch classes used",
-        'major_scale': "",
-        'minor_scale': "",
-        'pitch_entropy': "Entropy of pitches (measure of randomness)",
-        'pitch_class_entropy': "Entropy of pitch classes (measure of randomness)",
-        'pitch_class_histogram': 'pitch_class_histogram',
-        'chroma': 'chroma',
+        'major_scale': "major_scale[0] - most probably major scale, major_scale[1] - probability of that scale",
+        'minor_scale': "minor_scale[0] - most probably minor scale, minor_scale[1] - probability of that scale",
+        'pitch_entropy': "Entropy of pitches (measure of randomness). The greater the entropy value, the greater the pitch variation",
+        'pitch_class_entropy': "Entropy of pitch classes (measure of randomness). The greater the entropy value, the greater the pitch classes variation",
+        'pitch_class_histogram': 'A histogram of the proportions of each sound class to all sounds occurring in the piece. Visualization possible with the plot_chromagram function',
+        'chroma': 'Chromogram - flattened for all instruments occurring in the song at a given moment in time. Allows visualization and analysis of pitch distribution over time. Visualization possible with the plot_pitch_class_histogram function ',
     }
 
     pitch_metrics = {
         'pitch_range': pitch_range,
-        'pitch_range_tuple': pitch_range_tuple,
         'n_pitches_used': n_pitches_used,
         'n_pitch_classes_used': n_pitch_classes_used,
-        'major_scale': major_scale,
-        'minor_scale': minor_scale,
+        'major_scale': major_class_scale,
+        'minor_scale': minor_class_scale,
         'pitch_entropy': pitch_entropy,
         'pitch_class_entropy': pitch_class_entropy,
         'pitch_class_histogram': pitch_class_histogram,
--- music_metrics/src/rythm_metrics.py
+++ music_metrics/src/rythm_metrics.py
@@ -2,29 +2,24 @@ import muspy
 import pypianoroll
 from prettytable import PrettyTable
 
-from utils import load_representations
-
+from .utils import load_representations
 
 def get_rythm_metrics(data: any):
-    muspy_representation, midi_representation, pianoroll_representation = load_representations(
-        data)
+    
+    muspy_representation, midi_representation, pianoroll_representation = load_representations(data)
 
-    empty_beat_rate = drum_in_pattern_rate = drum_pattern_consistency = groove_consistency = \
-        empty_measure_rate = tempo_change_times = tempo = n_times_tempo_change = end_time = \
-        tempos = probabilities = estimate_tempo = beats = beat_start = downbeats = n_beats = \
-        onsets = n_notes = time_signatures = n_signatures = qualified_note_rate = None
+    empty_beat_rate = drum_in_pattern_rate_duple = drum_in_pattern_rate_triple = drum_pattern_consistency = groove_consistency = \
+    tempo_change_times = tempo = n_times_tempo_change = end_time = \
+    tempos = probabilities = estimate_tempo = beats = beat_start = downbeats = n_beats = \
+    onsets = n_notes = time_signatures = n_signatures = qualified_note_rate = None
 
     # muspy
     if muspy_representation:
         empty_beat_rate = muspy.empty_beat_rate(music=muspy_representation)
-        drum_in_pattern_rate = muspy.drum_in_pattern_rate(
-            music=muspy_representation, meter='duple')  # meter in ['duple', 'triple']
-        drum_pattern_consistency = muspy.drum_pattern_consistency(
-            music=muspy_representation)
-        groove_consistency = muspy.groove_consistency(
-            music=muspy_representation, measure_resolution=1)
-        empty_measure_rate = muspy.empty_measure_rate(
-            music=muspy_representation, measure_resolution=1)
+        drum_in_pattern_rate_duple = muspy.drum_in_pattern_rate(music=muspy_representation, meter='duple') # meter in ['duple', 'triple']
+        drum_in_pattern_rate_triple = muspy.drum_in_pattern_rate(music=muspy_representation, meter='duple') # meter in ['duple', 'triple']
+        drum_pattern_consistency = muspy.drum_pattern_consistency(music=muspy_representation)
+        groove_consistency = muspy.groove_consistency(music=muspy_representation, measure_resolution=4)
 
     # pretty_midi
     if midi_representation:
@@ -43,40 +38,40 @@ def get_rythm_metrics(data: any):
         n_signatures = len(time_signatures)
 
     # pypianoroll
-    # if pianoroll_representation:
-    #     qualified_note_rate = pypianoroll.qualified_note_rate(pianoroll_representation)
+    # if pianoroll_representation.any():
+    #     qualified_note_rate = pypianoroll.qualified_note_rate(pianoroll_representation, threshold=1)
 
     metrics_table = PrettyTable()
     metrics_table.field_names = ['Metric', 'Value', 'Description']
 
     metric_descriptions = {
-        'empty_beat_rate': '',
-        'drum_in_pattern_rate': '',
-        'drum_pattern_consistency': '',
-        'groove_consistency': '',
-        'empty_measure_rate': '',
-        'tempo_changes': '',
-        'n_times_tempo_change': '',
-        'end_time': '',
-        'estimate_tempi': '',
-        'estimate_tempo': '',
-        'beats': '',
-        'beat_start': '',
-        'downbeats': '',
-        'n_beats': '',
-        'onsets': '',
-        'n_notes': '',
-        'time_signatures': '',
-        'n_signatures': '',
-        'qualified_note_rate': '',
+        'empty_beat_rate': 'The proportion of empty bars in a song to the total number of bars.',
+        'drum_in_pattern_rate_duple': 'The ratio of percussion notes that fit a specific rhythmic pattern (duple) to the total number of percussion notes in the song. Only percussion tracks are taken into account.',
+        'drum_in_pattern_rate_triple': 'The ratio of percussion notes that fit a specific rhythmic pattern (triple) to the total number of percussion notes in the song. Only percussion tracks are taken into account.',
+        'drum_pattern_consistency': 'The largest value of the drum_in_pattern metric. Only percussion tracks are taken into account.',
+        'groove_consistency': 'Returns a floating-point value that determines the regularity and repeatability of the rhythm. The higher the value, the more regular the rhythm is. The value returned by metric can be in the range [0, 1]. The metric is only usable for songs that have a fixed meter and a minimum of two bars.',
+        'tempo_changes': 'A tuple with two elements, the first is an array with the time locations at which the rate changes, the second is an array with the rate values at changed at given times.',
+        'n_times_tempo_change': 'A value that tells how many times the tempo changes during a song.',
+        'end_time': 'Time of the song',
+        'estimate_tempi': 'A tuple with two element, the first is an array of potential tempos for a song in the bpm unit, the second is an array with probability of how well the tempo matches the song.',
+        'estimate_tempo': 'The tempo of the song with the highest probability of matching the song',
+        'beats': 'An array of note time locations depending on the song\'s meter, e.g. for the 6/8 meter it returns the third and sixth eighth notes, while for the 4/4 meter it returns each quarter note.',
+        'beat_start': 'The time location of the beginning of the song',
+        'downbeats': 'An array of first beats in bars expressed in seconds',
+        'n_beats': 'Number of bars with downbeat',
+        'onsets': 'An array with all notes in song',
+        'n_notes': 'Number of all notes in song',
+        'time_signatures': 'An array of musical meter changes in a song, along with timestamps of their occurrences.',
+        'n_signatures': 'Number of musical meter changes',
+        # 'qualified_note_rate': 'The metric returns the proportion of notes considered qualitative i.e. those that last longer than the declared threshold to all notes available in the piece.',
     }
 
     rythm_metrics = {
         'empty_beat_rate': empty_beat_rate,
-        'drum_in_pattern_rate': drum_in_pattern_rate,
+        'drum_in_pattern_rate_duple': drum_in_pattern_rate_duple,
+        'drum_in_pattern_rate_triple': drum_in_pattern_rate_triple,
         'drum_pattern_consistency': drum_pattern_consistency,
         'groove_consistency': groove_consistency,
-        'empty_measure_rate': empty_measure_rate,
         'tempo_changes': (tempo_change_times, tempo),
         'n_times_tempo_change': n_times_tempo_change,
         'end_time': end_time,
@@ -90,7 +85,7 @@ def get_rythm_metrics(data: any):
         'n_notes': n_notes,
         'time_signatures': time_signatures,
         'n_signatures': n_signatures,
-        'qualified_note_rate': qualified_note_rate,
+        # 'qualified_note_rate': qualified_note_rate,
     }
 
     for metric, value in rythm_metrics.items():
