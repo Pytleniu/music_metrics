00ba1273b62b2872a029d8b01cbe763b7f25baa2
Wiktor Pytlewski <wiktorpytlewski@gmail.com>
2024-01-12 23:27:47 +0100

    chore: update setup.py with project-specific information

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='music_metrics',
     version='0.1',
-    author='Your Name',
-    author_email='your.email@example.com',
-    description='A brief description of your project',
+    author='Michał Kopeć and Wiktor Pytlewski',
+    author_email='wiktor.pytlewski.stud@pw.edu.pl or michal.kopec6.stud@pw.edu.pl',
+    description='Comprehensive library aggregating standard and custom music metrics for insightful music analysis',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/music_metrics',
+    url='https://gitlab-stud.elka.pw.edu.pl/wpytlew1/zprp-projekt-zespol-16',
     packages=find_packages(include=['music_metrics', 'music_metrics.*']),
     install_requires=[
         'muspy',
@@ -23,5 +23,5 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.10',
 )
